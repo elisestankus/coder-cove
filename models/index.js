154a19af5cf59015e58cb5d1d2431498c205cb95
estan <elise.stankus@gmail.com>
2024-01-16 12:39:47 -0800

    added user-comment association

--- models/index.js
+++ models/index.js
@@ -10,3 +10,12 @@ User.hasMany(BlogPost, {
 BlogPost.belongsTo(User, {
     foreignKey: 'user_id'
 });
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
