0069abb087588de2aad43dedd212c507cc17252a
estan <elise.stankus@gmail.com>
2024-01-16 12:39:01 -0800

    added user-blogpost association

--- models/index.js
+++ models/index.js
@@ -2,3 +2,11 @@ const User = require('./User');
 const BlogPost = require('./BlogPost');
 const Comment = require('./Comment');
 
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+BlogPost.belongsTo(User, {
+    foreignKey: 'user_id'
+});
