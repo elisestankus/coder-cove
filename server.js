fa5475922797cf3ec6cc2bfc2872682c5d423f34
estan <elise.stankus@gmail.com>
2024-01-18 07:44:31 -0800

    added handlebars and setup handlebars as template engine

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//require dependencies
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
@@ -5,5 +6,13 @@ const exphbs = require('express-handlebars');
 const routes = require('./controller');
 const sequelize = require('./config/connection');
 
+//setup express app
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//create handlebars engine object
+const hbs = exphbs.create({});
+
+//sets up handlebars as template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
