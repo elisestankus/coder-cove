a3d6f2ee6afce0a02c2d28d616d5a8ee952f8af7
estan <elise.stankus@gmail.com>
2024-01-18 08:03:30 -0800

    told app to use routes, started server with sequelize.sync

--- server.js
+++ server.js
@@ -23,3 +23,14 @@ app.use(express.urlencoded({ extended: true }));
 
 //Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+//start server to begin listening
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    )
+  );
+});
