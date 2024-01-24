5df03f7eba36a5c8478bf2d769aa1da70429c902
estan <elise.stankus@gmail.com>
2024-01-24 11:53:57 -0800

    added get routes for homepage, login page, and signup page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,3 +2,30 @@
 const router = require('express').Router();
 const { BlogPost, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
+
+//get route for homepage
+router.get('/', async (req, res) => {
+    try {
+        res.render('homepage');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//get route for login page
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//get route for signup page
+router.get('/signup', async (req, res) => {
+    try {
+        res.render('signup');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
