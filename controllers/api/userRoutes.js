c027f8733b9ab0767205144fe39283fa2d18bedd
estan <elise.stankus@gmail.com>
2024-01-24 16:57:52 -0800

    added post route to login user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,6 +18,39 @@ router.post('/', async (req, res) => {
     }
 });
 
+//post route to login user
+
+router.post('/login', async (req, res) => {
+    try {
+        // find user matching the inputted email
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+        // check inputted password against password stored in database
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+        // create new session
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 
 
