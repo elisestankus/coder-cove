c1b5d4c21e30e2b8db687c5f4203ebb2274675c4
estan <elise.stankus@gmail.com>
2024-01-25 11:04:10 -0800

    added post route

--- controllers/api/blogpostRoutes.js
+++ controllers/api/blogpostRoutes.js
@@ -3,7 +3,18 @@ const router = require('express').Router();
 const { BlogPost } = require('../../models');
 
 //post route to create new blog post
-
+router.post('/', async (req, res) => {
+    try {
+        const postData = await BlogPost.create({
+            user_id: req.session.user_id,
+            title: req.body.title,
+            content: req.body.content,
+        });
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err)
+    }
+});
 
 //put route to update blog post
 
