80b6de378a9ae2fa9203ccccd38c358b3a181ec4
estan <elise.stankus@gmail.com>
2024-01-25 10:59:24 -0800

    created blogpostRoutes.js. required router and blogpost model

---
+++ controllers/api/blogpostRoutes.js
@@ -0,0 +1,11 @@
+//dependencies
+const router = require('express').Router();
+const { BlogPost } = require('../../models');
+
+//post route to create new blog post
+
+
+//put route to update blog post
+
+
+//delete route to delete blog post
