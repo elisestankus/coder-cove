2f58d49eab7f6ff080ec5505d2fc1eac9fab5b0a
estan <elise.stankus@gmail.com>
2024-01-25 11:18:14 -0800

    added put and delete routes. exported router

--- controllers/api/blogpostRoutes.js
+++ controllers/api/blogpostRoutes.js
@@ -16,7 +16,46 @@ router.post('/', async (req, res) => {
     }
 });
 
-//put route to update blog post
+//put route to update blog post by id
+router.put('/:id', async (req, res) => {
+    try {
+        const postData = await BlogPost.update(
+            {
+                title: req.body.title,
+                content: req.body.content,
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            }
+        );
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
+//delete route to delete blog post by id
+router.delete('/:id', async (req, res) => {
+    try {
+       const postData = await BlogPost.destroy({
+        where: {
+            id: req.params.id,
+        },
+       });
+       if (!postData) {
+        res.status(404).json({message: 'No post found with this id!'});
+        return;
+       } 
+       res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
-//delete route to delete blog post
+module.exports = router;
