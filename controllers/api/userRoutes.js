492f921b382c31e25925f89cc2147a25b22400d7
estan <elise.stankus@gmail.com>
2024-01-24 16:54:02 -0800

    added post route to signup/create new user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,5 +2,22 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//post route to signup a new user & add to db
+router.post('/', async (req, res) => {
+    try {
+        //create new user
+        const userData = await User.create(req.body);
+        //create new session
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
 
 
