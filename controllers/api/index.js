8ce50288f87ede43101f1b64f610210d24024717
estan <elise.stankus@gmail.com>
2024-01-25 11:39:35 -0800

    required router and routes files

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+//dependencies/files
+const router = require('express').Router();
+const loginRoutes = require('./loginRoutes');
+const signupRoutes = require('./signupRoutes');
+const forgotPasswordRoutes = require('./forgotpasswordRoute');
+const languageRoutes = require('./languageRoutes');
+const projectRoutes = require('./projectRoutes');
+
