1b2e1b0c2cc1f671de7bcc375a27039300d66387
estan <elise.stankus@gmail.com>
2024-01-30 21:32:57 -0800

    added signup form handler

---
+++ public/js/signup.js
@@ -0,0 +1,21 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#signupUsername').value.trim();
+    const email = document.querySelector('#signupEmail').value.trim();
+    const password = document.querySelector('#signupPassword').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/user/', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
+};
