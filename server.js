52d18b68daed74d4fd3c78acfd8881a57799e451
estan <elise.stankus@gmail.com>
2024-02-03 12:18:53 -0800

    required helpers and passed through to handlebars engine

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -29,7 +30,7 @@ const sess = {
 app.use(session(sess));
 
 //create handlebars engine object
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 //sets up handlebars as template engine
 app.engine('handlebars', hbs.engine);
